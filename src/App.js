5d2feb8892ae035d55940041fc4371a91ca6d447
Choxy5 <kristijan.blazevic000@gmail.com>
2021-10-20 00:02:41 +0200

    Navbar Design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './components';
 
 export default function App() {
   return (
     <div className="app">
-      <h1>Hello world</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.scss';
+import { IoBagOutline } from 'react-icons/io5';
+import { BiUser } from 'react-icons/bi';
+
+export function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="left-section">
+          <h1>eCom.</h1>
+        </div>
+        <div className="right-section">
+          <IoBagOutline style={{ marginRight: '20px', fontSize: '25px' }} />
+          <BiUser style={{ fontSize: '25px' }} />
+        </div>
+      </div>
+    </div>
+  );
+}
